98c564b0e429ef914ac645349ea8876e49d891e6
Axel Laise <axellaise53@gmail.com>
2026-01-13 16:15:36 +0100

    Fin partie 4

--- TP2/index.py
+++ TP2/index.py
@@ -52,30 +52,50 @@ class Index():
                     description_index[f"{token}"] = [line["url"]]
         return description_index
     
+    def extract_reviews_info(self, line):
+        product_reviews = line["product_reviews"]
+        if product_reviews != []:
+            number_of_rating = len(product_reviews)
+            product_reviews = sorted(product_reviews, key=lambda x: x["date"], reverse=True)
+            last_rating = product_reviews[0]["rating"]
+            avg_rating = 0
+            for review in product_reviews:
+                avg_rating += int(review["rating"])
+            avg_rating = avg_rating/number_of_rating
+        else:
+            number_of_rating = 0
+            last_rating = 0
+            avg_rating = 0
+        return number_of_rating, last_rating, avg_rating
+
     def create_reviews_index(self):
         reviews_index = {}
         for line in self.data:
-            product_reviews = line["product_reviews"]
-            if product_reviews != []:
-                number_of_rating = len(product_reviews)
-                product_reviews = sorted(product_reviews, key=lambda x: x["date"], reverse=True)
-                last_rating = product_reviews[0]["rating"]
-                avg_rating = 0
-                for review in product_reviews:
-                    avg_rating += int(review["rating"])
-                avg_rating = avg_rating/number_of_rating
-            else:
-                number_of_rating = 0
-                last_rating = 0
-                avg_rating = 0
+            number_of_rating, last_rating, avg_rating = self.extract_reviews_info(line)
             reviews_index[line["url"]] = {"total_reviews": number_of_rating, "mean_mark": avg_rating, "last_rating": last_rating}
         return reviews_index
 
+    def create_feature_index(self, feature):
+        feature_index = {}
+        for line in self.data:
+            if feature in line["product_features"].keys():
+                feature_text = line["product_features"][feature]
+                cleaned_featured = self.tokenize_and_clean_text(feature_text)
+                for token in cleaned_featured:
+                    if token in feature_index.keys():
+                        feature_index[f"{token}"].append(line["url"])
+                    else:
+                        feature_index[f"{token}"] = [line["url"]]
+        return feature_index
 
-
+#### Features a ajouté, flavor, material
 
 index = Index("TP2/products.jsonl")
 title_index = index.create_title_index()
 description_index = index.create_description_index()
 reviews_index = index.create_reviews_index()
-
+brand_index = index.create_feature_index("brand")
+origin_index = index.create_feature_index("made in")
+flavor_index = index.create_feature_index("flavor")
+material_index = index.create_feature_index("material")
+print(material_index)
